a73c5f5580ecd103788bdd3157a8905e5fea3bf4
Miu Razvan <miu.razvan28@gmail.com>
2025-02-06 20:37:56 +0200

    update

--- setup.py
+++ setup.py
@@ -36,11 +36,12 @@ setup(
     url='https://github.com/eea/eea.plotly',
     license='GPL version 2',
     packages=find_packages(exclude=['ez_setup']),
-    namespace_packages=['eea'],
+    namespace_packages=['eea', 'eea.plotly'],
     include_package_data=True,
     zip_safe=False,
     install_requires=[
         'setuptools',
+        "plone.volto",
         # -*- Extra requirements: -*-
     ],
     extras_require={
