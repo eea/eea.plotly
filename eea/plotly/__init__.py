de696d01354ee3377cd0f68c89b70fb1d744ec70
Miu Razvan <miu.razvan28@gmail.com>
2025-02-06 20:42:10 +0200

    update

--- eea/plotly/__init__.py
+++ eea/plotly/__init__.py
@@ -1,6 +1,8 @@
 """ Main product initializer
 """
 from zope.i18nmessageid.message import MessageFactory
+__import__('pkg_resources').declare_namespace(__name__)
+
 EEAMessageFactory = MessageFactory('eea')
 
 
