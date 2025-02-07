c66085e8797a5175697e570f58ab64d152fa11af
Miu Razvan <miu.razvan28@gmail.com>
2025-02-07 11:38:45 +0200

    permission typo

--- eea/plotly/interfaces.py
+++ eea/plotly/interfaces.py
@@ -10,70 +10,7 @@ TEMPLATES_SCHEMA = json.dumps({
     "type": "array",
     "items": {
         "type": "object",
-        "properties": {
-            "label": {
-                "type": "string",
-                "title": _("Label"),
-                "description": _("A descriptive label for the template."),
-                "default": ""
-            },
-            "type": {
-                "type": "string",
-                "title": _("Type"),
-                "description": _("The type of template."),
-                "default": ""
-            },
-            "visualization": {
-                "type": "object",
-                "title": _("Visualization"),
-                "description": _("The visualization data."),
-                "properties": {
-                    "chartData": {
-                        "type": "object",
-                        "properties": {
-                            "data": {
-                                "type": "array",
-                                "items": {
-                                    "type": "object",
-                                    "properties": {}
-                                }
-                            },
-                            "layout": {
-                                "type": "object",
-                                "properties": {}
-                            },
-                            "frames": {
-                                "type": "array",
-                                "items": {
-                                    "type": "object",
-                                    "properties": {}
-                                }
-                            },
-                        }
-                    },
-                    "use_data_sources": {
-                        "type": "boolean",
-                        "default": True
-                    },
-                    "data_source": {
-                        "type": "object",
-                        "properties": {}
-                    },
-                    "provider_url": {
-                        "type": "string",
-                        "default": ""
-                    }
-                },
-                "default": {
-                    "chartData": {
-                        "data": [],
-                        "layout": {},
-                        "frames": []
-                    },
-                    "use_data_sources": True
-                }
-            }
-        }
+        "properties": {}
     }
 })
 
@@ -92,5 +29,5 @@ class IPlotlySettings(Interface):
         schema=TEMPLATES_SCHEMA,
         default=[],
         widget="plotly_templates",
-        required=True
+        required=False
     )
