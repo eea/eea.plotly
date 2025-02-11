2235e52989dd24f956cbc1e6955c70d7a0d864cb
Alexandru Ghica <alec.ghica@eaudeweb.ro>
2025-02-11 13:44:48 +0200

    Update __init__.py

--- eea/plotly/__init__.py
+++ eea/plotly/__init__.py
@@ -1,7 +1,6 @@
 """ Main product initializer
 """
 from zope.i18nmessageid.message import MessageFactory
-#__import__('pkg_resources').declare_namespace(__name__)
 
 EEAMessageFactory = MessageFactory('eea')
 
@@ -9,6 +8,3 @@ EEAMessageFactory = MessageFactory('eea')
 def initialize(context):
     """Initializer called when used as a Zope 2 product.
     """
-
-
-__all__ = ['initialize']
