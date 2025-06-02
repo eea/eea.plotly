14e4b686b1717764bb4efcc005003a6a79f820b9
Miu Razvan <miu.razvan28@gmail.com>
2025-06-02 13:02:59 +0300

    add cp initial settings on install + reset endpoint

---
+++ eea/plotly/restapi/controlpanel/reset.py
@@ -0,0 +1,42 @@
+"""RestAPI enpoint @plotly GET"""
+import os.path
+import json
+from plone.restapi.services import Service
+from plone.registry.interfaces import IRegistry
+from plone.protect.interfaces import IDisableCSRFProtection
+from zope.interface import implementer, alsoProvides
+from zope.component import getUtility
+from zope.publisher.interfaces import IPublishTraverse
+
+from eea.plotly.controlpanel import IPlotlySettings
+
+
+@implementer(IPublishTraverse)
+class PlotlySettingsReset(Service):
+    """Plotly Settings Reset"""
+
+    def reply(self):
+        """Reply"""
+        # Disable CSRF protection
+        alsoProvides(self.request, IDisableCSRFProtection)
+
+        registry = getUtility(IRegistry)
+        cp = registry.forInterface(IPlotlySettings)
+        package_dir = os.path.dirname(
+            os.path.dirname(os.path.abspath(__file__)))
+        profile_path = os.path.join(
+            package_dir, '..', 'profiles', 'default', 'controlpanel.json')
+
+        if os.path.exists(profile_path):
+            with open(profile_path, 'r') as f:
+                settings = json.load(f)
+                # Reset control panel with default settings
+                cp.themes = settings.get('themes', [])
+                cp.templates = settings.get('templates', [])
+        else:
+            print(f"File not found: {profile_path}")
+
+        return {
+            "themes": cp.themes,
+            "templates": cp.templates
+        }
--- eea/plotly/setuphandlers.py
+++ eea/plotly/setuphandlers.py
@@ -1,7 +1,12 @@
 """ Custom setup
 """
+import os.path
+import json
 from Products.CMFPlone.interfaces import INonInstallable
+from plone.registry.interfaces import IRegistry
 from zope.interface import implementer
+from zope.component import getUtility
+from eea.plotly.controlpanel import IPlotlySettings
 
 
 @implementer(INonInstallable)
@@ -21,6 +26,20 @@ def post_install(context):
     """ Post install script
     """
     # Do something at the end of the installation of this package.
+    package_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    profile_path = os.path.join(
+        package_dir, 'plotly', 'profiles', 'default', 'controlpanel.json')
+
+    if os.path.exists(profile_path):
+        with open(profile_path, 'r') as f:
+            settings = json.load(f)
+            # Initialize control panel with default settings
+            registry = getUtility(IRegistry)
+            cp = registry.forInterface(IPlotlySettings)
+            cp.themes = settings.get('themes', [])
+            cp.templates = settings.get('templates', [])
+    else:
+        print(f"File not found: {profile_path}")
 
 
 def uninstall(context):
