f043bc6b8f1d51ac99c76df6b782e1f25d0a741d
Miu Razvan <miu.razvan28@gmail.com>
2025-07-03 13:32:55 +0300

    pylint fix

--- eea/plotly/io_json.py
+++ eea/plotly/io_json.py
@@ -1,9 +1,12 @@
+"""JSON encoder that handles Decimal values."""
 import decimal
 import json
 
 
 class JSONEncoder(json.JSONEncoder):
+    """JSON encoder that handles Decimal values."""
+
     def default(self, o):
         if isinstance(o, decimal.Decimal):
             return str(o)
-        return super(JSONEncoder, self).default(o)
+        return super().default(o)
